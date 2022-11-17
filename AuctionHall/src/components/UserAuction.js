70503b7616bddd6a222cba467fec6e14594f7dd5
Bernard-Calma <nard.calma@gmail.com>
2022-11-16 19:20:59 -0800

    removed title and added cancel text to remove auction

--- AuctionHall/src/components/UserAuction.js
+++ AuctionHall/src/components/UserAuction.js
@@ -4,12 +4,18 @@ import { StyleSheet, Text, View, Dimensions } from "react-native"
 export const UserAuction = (props) => {
     return(
         <View style = {styles.containter}>
-            <Text>{props.auction.title}</Text>
+            <Text style = {styles.cancel}>Cancel</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    cancel:{
+        fontFamily: "copperplate",
+        fontWeight: "500",
+        fontSize: 20,
+        margin: 5
+    },
     containter: {
         // borderWidth: 1,
         // borderColor: 'blue',
@@ -17,5 +23,7 @@ const styles = StyleSheet.create({
         backgroundColor: "gray",
         marginBottom: 5,
         height: Dimensions.get('window').height / 4.22,
+        flexDirection: "row",
+        justifyContent: "flex-end"
     },
 })
